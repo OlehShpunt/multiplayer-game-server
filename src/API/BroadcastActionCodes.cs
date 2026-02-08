9a85b7990209e13379ffec66568248b1fa38829e
OlehShpunt <olehshpunt2@gmail.com>
2026-02-08 00:37:34 -0800

    feat: add BinaryMessageBuilder for sending custom binary messages to clients

---
+++ src/API/BroadcastActionCodes.cs
@@ -0,0 +1,11 @@
+namespace API;
+
+public enum BroadcastActionCodes
+{
+    Error = -1,
+    Success = 0,
+    PlayerJoinedLobby = 1,
+    PlayerLeftLobby = 2,
+    PlayerMoved = 3,
+    PlayerTeleported = 4,
+}
